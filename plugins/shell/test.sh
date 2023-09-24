6cff16e94240946bc4a584174e80819f42fd5b06
JAMIEKIMHS <yumisgood@gmail.com>
2023-09-24 13:12:15 +0900

    plugins 폴더 추가

---
+++ plugins/shell/test.sh
@@ -0,0 +1,9 @@
+if [ $1 == APPLE ];then
+	echo "You selected Apple!"
+elif [ $1 == ORANGE ];then
+	echo "You selected Orange!"
+elif [ $1 == GRAPE ];then
+	echo "You selected Grape!"
+else
+	echo "You selected other Fruit!"
+fi
